07c167cb3553e58d8341ce2fe33675edd8f470de
suhas991 <suhas4497@gmail.com>
2024-06-10 11:27:56 +0530

    selection section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import Header from './components/Header'
-
+import Content from './components/Content'
 function App() {
 
   return (
     <>
      <Header/>
+     <Content/>
     </>
   )
 }
---
+++ src/components/Content.jsx
@@ -0,0 +1,15 @@
+import './Content.css'
+export default function Content(){
+
+    return <>
+    <div className="container">
+         <div className="types">
+            <div><button>All</button></div>
+            <div><button>Breakfast</button></div>
+            <div><button>Lunch</button></div>
+            <div><button>Dinner</button></div>
+         </div>
+       </div>
+    </>
+    
+}
